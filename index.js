b334cf3aa0ce8ab18b44102a3db2514bac2cb626
Sk Shariful Islam <skshariful10@gmail.com>
2023-05-19 23:32:39 +0600

    dotenv Added

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+console.log(process.env.DB_USER)
+
+
+
 
 app.get("/", (req, res) => {
 	res.send("Baby Toy is running");
