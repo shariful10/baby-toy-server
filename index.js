d530dedcbc2d369a5b1f3ebf4c3b40ad73fecf7c
Sk Shariful Islam <skshariful10@gmail.com>
2023-05-21 14:21:26 +0600

    Use Query To Get Specific Json Data From MongoDB Database

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
@@ -35,6 +35,9 @@ async function run() {
 
 		app.get("/toys/:id", async (req, res) => {
 			const id = req.params.id;
+			const query = {_id: new ObjectId(id)};
+			const result = await toyCollection.findOne(query);
+			res.send(result);
 		})
 
 		// Send a ping to confirm a successful connection
